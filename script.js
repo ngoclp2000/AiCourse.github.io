8b387d1907c75e38f8da10cfed8ebea64f8e80ed
ngoclp2000 <50916219+ngoclp2000@users.noreply.github.com>
2019-10-23 21:41:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-var Board;
+let Board;
 const humans = 'X';
 const AIplayer = 'O';
 var sizeOfBoard,cells;
-var delayInMilliseconds = 1000; //1 second
+var delayInMilliseconds = 0; //1 second
 let BoardStart;
+let winnerTemp;
 const winDirection1 =[
     [0 ,1 ,2],
     [3, 4, 5],
@@ -15,35 +16,20 @@ const winDirection1 =[
     [2, 4, 6]
 ];
 const winDirection2 =[
-    [0,1,2,3],
-    [1,2,3,4],
-    [5,6,7,8],
-    [6,7,8,9],
-    [10,11,12,13],
-    [11,12,13,14],
-    [15,16,17,18],
-    [16,17,18,19],
-    [20,21,22,23],
-    [21,22,23,24],
-    [0,5,10,15],
-    [5,10,15,20],
-    [1,6,11,16],
-    [6,11,16,21],
-    [2,7,12,17],
-    [7,12,17,22],
-    [3,8,13,18],
-    [8,13,18,23],
-    [4,9,14,19],
-    [9,14,19,24],
-    [1,7,13,19],
-    [0,6,12,18],
-    [6,12,18,24],
-    [5,11,17,23],
-    [3,7,11,15],
-    [4,8,12,16],
-    [8,12,16,20],
-    [9,13,17,21]
+    [0,1,2,3,4],
+    [5,6,7,8,9],
+    [10,11,12,13,14],
+    [15,16,17,18,19],
+    [20,21,22,23,24],
+    [0,5,10,15,20],
+    [1,6,11,16,21],
+    [2,7,12,17,22],
+    [3,8,13,18,23],
+    [4,9,14,19,24],
+    [0,6,12,18,24],
+    [4,8,12,16,20]
 ];
+
 let choice_win_direct;
 function SizeofBoard(number){
     sizeOfBoard = number;
@@ -68,7 +54,6 @@ function SizeofBoard(number){
 }
 
 function startGame(){
-    console.log(Math.pow(sizeOfBoard,2));
     Board = Array.from(Array(Math.pow(sizeOfBoard,2)).keys);
     for(let i = 0; i < Math.pow(sizeOfBoard,2); i++){
         cells[i].innerText = "";
@@ -83,6 +68,7 @@ function startGame(){
 }
 
 function turnclick(cell){
+    playSound();
     let gameWon = turn(cell.target.id,humans);
     if(!gameWon){
         document.querySelector("#playing").innerText = "Turn: Computer";
@@ -100,8 +86,8 @@ function turnclick(cell){
                     true_id[i].addEventListener('click',turnclick, false);
                 }
             }
-            turn(bestSpot(), AIplayer);
-        
+            turn(bestSpot(Board,AIplayer)[0], AIplayer);
+            playSound();
         document.querySelector("#playing").innerText = "Turn: Player";
              }, delayInMilliseconds);
             }
@@ -114,53 +100,50 @@ function turn(id, player){
     true_id[id].innerText = player;
     true_id[id].style.background = "red";
     true_id[id].removeEventListener('click', turnclick, false);
-    let gameWon = checkWin();
-    return gameWon;
-}
-function checkWin(){
-    var check = 0;
-    let winner;
-    console.log(choice_win_direct);
-    console.log(Board);
-    choice_win_direct.forEach(element => {
-           if(sizeOfBoard == 3){
-               if(Board[element[0]] == humans && Board[element[1]] == humans && Board[element[2]] == humans){
-                   check = 1;
-                   winner = humans;
-               }
-               else if(Board[element[0]] == AIplayer && Board[element[1]] == AIplayer && Board[element[2]] == AIplayer){
-                check = 1;
-                winner = AIplayer;
-            }
-           }else{
-            //console.log(Board[element[0]],Board[element[1]],Board[element[2]],Board[element[3]]);
-            if(Board[element[0]] == humans && Board[element[1]] == humans && Board[element[2]] == humans && Board[element[3]] == humans){
-                check = 1;
-                winner = humans;
-            }
-            else if(Board[element[0]] == AIplayer && Board[element[1]] == AIplayer &&  Board[element[2]] == AIplayer && Board[element[3]] == AIplayer){
-                check = 1;
-                winner = AIplayer;
-            }
-           }
-    });
-    if(check){
+    let gameWon = checkWin(Board);
+    if(gameWon){
         for(let i = 0 ;i < Math.pow(sizeOfBoard,2) ;i++){
             document.getElementById(i).removeEventListener('click', turnclick , false);
         } 
         document.querySelector("#playbut").innerText = "Replay";
-        declareWinner(winner);  
+        declareWinner(player); 
+    }
+    return gameWon;
+}
+function checkWin(Board1){
+    var check = 0;
+    let winner;
+    choice_win_direct.forEach(element => {
+           if(sizeOfBoard == 3){
+               if(Board1[element[0]] == humans && Board1[element[1]] == humans && Board1[element[2]] == humans){
+                   check = 1;
+                   winner = humans;
+                   winnerTemp = humans;
+               }
+               else if(Board1[element[0]] == AIplayer && Board1[element[1]] == AIplayer && Board1[element[2]] == AIplayer){
+                check = 1;
+                winner = AIplayer;
+                winnerTemp = AIplayer;
+            }
+           }else{
+            //console.log(Board[element[0]],Board[element[1]],Board[element[2]],Board[element[3]]);
+            if(Board1[element[0]] == humans && Board1[element[1]] == humans && Board1[element[2]] == humans && Board1[element[3]] == humans && Board1[element[4]] == humans){
+                check = 1;
+                winner = humans;
+                winnerTemp = humans;
+            }
+            else if(Board1[element[0]] == AIplayer && Board1[element[1]] == AIplayer &&  Board1[element[2]] == AIplayer && Board1[element[3]] == AIplayer && Board1[element[4]]== AIplayer){
+                check = 1;
+                winner = AIplayer;
+                winnerTemp = AIplayer;
+            }
+           }
+    });
+    if(check){ 
         return true;
     }
     else 
         return false;
-}
-function bestSpot(){
-    do{
-        let random_pos = Math.floor(Math.random() * Math.pow(sizeOfBoard,2));
-        if(Board[random_pos] == undefined)
-            return random_pos;
-    }while(1);
 }
 function checkTie(){
     for(let i = 0 ; i < Math.pow(sizeOfBoard,2) ;i++){
@@ -179,7 +162,7 @@ function declareWinner(player){
     document.getElementById("myNav").style.width = "100%";
     document.getElementById("winner-player").innerText =   player;
 }
-document.getElementById("content").style.height = innerHeight + "px";
+
 
 let menu = document.querySelector("#menu-starter");
 let button = document.querySelectorAll(".option-but");
@@ -205,4 +188,95 @@ function restart(){
 function backtoMenu(){
     document.getElementById("myNav").style.width = "0%";
     document.querySelector("#playing").style.display = "none";
+}
+var sound = new Audio();         // create the audio
+sound.src = "sound.wav";  // set the resource location 
+sound.oncanplaythrough = function(){   // When the sound has completely loaded
+    sound.readyToRock = true;    // flag sound is ready to play
+                                   // I just made it up and can be anything
+};
+sound.onerror = function(){      // not required but if there are problems this will help debug the problem
+    console.log("Sound file SoundFileURL.mp3 failed to load.")
+};
+function playSound(){
+    if(sound && sound.readyToRock){  // check for the sound and if it has loaded
+        sound.currentTime = 0;       // seek to the start
+        sound.play();                // play it till it ends
+    }
+}
+document.getElementById("content").style.height = innerHeight + "px";
+
+function find_min(list){
+    let min = Infinity;
+    let node_chose = [];
+    list.forEach(element=>{
+        if(element[1] < min){
+            node_chose = element;
+            min = element[1];
+        }
+    });
+    return node_chose;
+}
+function find_max(list){
+    let max = -Infinity;
+    let node_chose = [];
+    list.forEach(element=>{
+        if(element[1] > max){
+            node_chose = element;
+            max = element[1];
+        }
+    });
+    return node_chose;
+}
+function Find_available_node(_Board){
+    let list = [];
+    for(let i = 0 ; i < Math.pow(sizeOfBoard,2);i++){
+        if(_Board[i] === undefined)
+            list.push(i);
+    }
+    return list;
+}
+
+function _checkTie(_Board){
+    for(let i = 0 ; i < Math.pow(sizeOfBoard,2) ;i++){
+        if(_Board[i] === undefined){
+            return false;
+        }
+    }
+    return true;
+}
+function bestSpot(_Board,player){
+    if(Find_available_node(_Board).length <= 10){
+        return minmax(_Board,player,0)
+    }else{
+        do{
+            let random_pos = Math.floor(Math.random() * Math.pow(sizeOfBoard,2));
+            if(Board[random_pos] == undefined)
+                return [random_pos];
+        }while(1);
+    }
+}
+
+function minmax(_Board,player,depth){
+    let list = [[]];
+    let list_available_node =  Find_available_node(_Board);
+    list_available_node.forEach(element => {
+        let Board_copy = [..._Board];
+        Board_copy[element] = player;
+        if(checkWin(Board_copy))
+            if(player == AIplayer)
+                 list.push([element,10-depth]);
+            else 
+                list.push([element,depth-10]);
+        else if(_checkTie(Board_copy))
+                list.push([element,0]);
+        else 
+                list.push([element,minmax(Board_copy, player == AIplayer ? humans : AIplayer,depth+1)[1]]);
+    });
+    if(player == AIplayer){
+        return find_max(list);
+    }
+    else{
+        return find_min(list);
+    }
 }
